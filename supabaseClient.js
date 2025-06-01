292244f50527c25941e78bbad8ac927b08ca6e1f
cse23-016 <160720362+cse23-016@users.noreply.github.com>
2025-06-01 20:12:31 +0200

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,9 +1,9 @@
 // supabaseClient.js
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SUPABASE_URL = 'https://pnfuotqwdqnqswhklttv.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBuZnVvdHF3ZHFucXN3aGtsdHR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU0MDEwMTEsImV4cCI6MjA2MDk3NzAxMX0.Hsu6P7V4rBzyd2g3LndaCVKOL6TkOaGco3p3X6aFUgU'; // Get from Supabase Dashboard
+const supabaseUrl = 'https://pnfuotqwdqnqswhklttv.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBuZnVvdHF3ZHFucXN3aGtsdHR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU0MDEwMTEsImV4cCI6MjA2MDk3NzAxMX0.Hsu6P7V4rBzyd2g3LndaCVKOL6TkOaGco3p3X6aFUgU';
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
